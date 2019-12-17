eb33ba82027053abb4b906a866c199f643b067c6
pravirkr <pravirka@gmail.com>
2019-12-17 16:57:22 +1100

    have FitsReader now!

--- sigpyproc/Readers.py
+++ sigpyproc/Readers.py
@@ -12,6 +12,8 @@ from sigpyproc.Filterbank import Filterbank,FilterbankBlock
 from sigpyproc.TimeSeries import TimeSeries
 from sigpyproc.FourierSeries import FourierSeries
 
+from sigpyproc.PSRFits import SpectraInfo, unpack_2bit, unpack_4bit
+
 class FilReader(Filterbank):
     """Class to handle the reading of sigproc format filterbank files
     
@@ -134,6 +136,341 @@ class FilReader(Filterbank):
             yield int(block/self.header.nchans),int(ii),data
         if verbose:
             print("Execution time: %f seconds     \n"%(time.time()-tstart))
+
+
+
+class FitsReader(Filterbank):
+    """
+    Class to handle the reading of PSRFits format fits files
+    
+    :param filename: list of PSRFits files
+    :type filename: :func:`str`
+    
+    .. note::
+    
+       To be considered as a PSRFits format fits file the header must be
+       readable using Astropy pyfits package. 
+    """
+    def __init__(self, psrfitslist):
+        if isinstance(psrfitslist, str):
+            psrfitslist = list(psrfitslist)
+
+        psrfitsfn = psrfitslist[0]
+        if not os.path.isfile(psrfitsfn):
+            raise ValueError("ERROR: File does not exist!\n\t(%s)" % psrfitsfn)
+        self.filename = psrfitsfn
+        self.filelist = psrfitslist
+        self.fileid   = 0
+
+        self._fits    = pyfits.open(psrfitsfn, mode='readonly', memmap=True)
+
+        # Header 
+        self.specinfo = SpectraInfo(psrfitslist)
+        self.header   = self.specinfo.to_sigpyproc()
+
+        super(FitsReader,self).__init__()
+
+
+    def readBlock(self, start, nsamps, apply_weights=False, 
+                        apply_scales=False, apply_offsets=False):
+        """
+        Read a block of PSRFits data (and return in filterbank).
+
+        :param start: first time sample of the block to be read
+        :type start: int
+        :param nsamps: number of samples in the block (i.e. block will be nsamps*nchans in size)
+        :type nsamps: int
+        :return: 2-D array of filterbank data
+        :rtype: :class:`~sigpyproc.Filterbank.FilterbankBlock`
+        """
+        # Calculate starting subint and ending subint
+        startsub = int(start / self.specinfo.spectra_per_subint)
+        skip     = int(start - (startsub * self.specinfo.spectra_per_subint))
+        endsub   = int((start + nsamps) / self.specinfo.spectra_per_subint)
+        trunc    = int(((endsub + 1) * self.specinfo.spectra_per_subint) - (start + nsamps))
+
+        # sort of cread (#TODO if possible)
+        # Read full subints (need to be more fast #TODO)
+        data     = self.get_data(self, startsub, endsub, apply_weights=apply_weights, 
+                                    apply_scales=apply_scales, apply_offsets=apply_offsets)
+        # data shape is (nchan, nsample)
+        # Truncate data to desired interval
+        if trunc > 0:
+            data = data[:, skip:-trunc]
+        elif trunc == 0:
+            data = data[:, skip:]
+        else:
+            raise ValueError("Number of bins to truncate is negative: %d" % trunc)
+
+        start_mjd  = self.header.mjdAfterNsamps(start)
+        new_header = self.header.newHeader({'tstart':start_mjd})
+        return FilterbankBlock(data, new_header)
+
+
+
+    def readPlan(self,gulp,skipback=0,start=0,nsamps=None,verbose=True):
+        """
+        A generator used to perform PSRFits reading.
+ 
+        :param gulp: number of samples in each read
+        :type gulp: int
+
+        :param skipback: number of samples to skip back after each read (def=0)
+        :type skipback: int
+
+        :param start: first sample to read from filterbank (def=start of file)
+        :type start: int
+
+        :param nsamps: total number samples to read (def=end of file)
+        :type nsamps: int
+
+        :param verbose: flag for display of reading plan information (def=True)
+        :type verbose: bool
+
+        :return: An generator that can read through the file.
+        :rtype: generator object
+        
+        .. note::
+
+           For each read, the generator yields a tuple ``x``, where:
+           
+              * ``x[0]`` is the number of samples read
+              * ``x[1]`` is the index of the read (i.e. ``x[1]=0`` is the first read)
+              * ``x[2]`` is a 1-D numpy array containing the data that was read
+
+           The normal calling syntax for this is function is:
+
+           .. code-block:: python
+           
+              for nsamps, ii, data in self.readPlan(*args,**kwargs):
+                  # do something
+
+           where data always has contains ``nchans*nsamps`` points. 
+
+        """
+        # Make gulp multiple of spectra_per_subint (clean way)
+
+
+        if nsamps is None:
+            nsamps = self.header.nsamples-start
+        if nsamps<gulp:
+            gulp = nsamps
+
+        tstart = time.time()
+        skipback = abs(skipback)
+        if skipback >= gulp:
+            raise ValueError("readsamps must be > skipback value")
+
+
+        self._file.seek(self.header.hdrlen+start*self.sampsize)
+
+
+        nreads   = nsamps//(gulp-skipback)
+        lastread = nsamps-(nreads*(gulp-skipback))
+        if lastread<skipback:
+            nreads -= 1
+            lastread = nsamps-(nreads*(gulp-skipback))
+        blocks = [(ii, gulp, -skipback) for ii in range(nreads)]
+        blocks.append((nreads, lastread, 0))
+        
+        if verbose:
+            print()
+            print("PSRFits reading plan:")
+            print("------------------------")
+            print("Called on file:       ",self.filename)      
+            print("Called by:            ",istack()[1][3])
+            print("Number of samps:      ",nsamps)
+            print("Number of reads:      ",nreads)
+            print("Nsamps per read:      ",blocks[0][1])
+            print("Nsamps of final read: ",blocks[-1][1])
+            print("Nsamps to skip back:  ",-1*blocks[0][2])
+            print()
+        
+        for ii,block,skipback in blocks:
+            if verbose:
+                stdout.write("Percentage complete: %d%%\r"%(100*ii/nreads))
+                stdout.flush()
+
+            # Calculate starting subint and ending subint
+            startsub = int(start / self.specinfo.spectra_per_subint)
+            skip     = int(start - (startsub * self.specinfo.spectra_per_subint))
+            endsub   = int((start + block) / self.specinfo.spectra_per_subint)
+            trunc    = int(((endsub + 1) * self.specinfo.spectra_per_subint) - (start + block))
+
+            # sort of cread (#TODO if possible)
+            # Read full subints (need to be more fast #TODO)
+            data     = self.get_data(self, startsub, endsub, apply_weights=apply_weights, 
+                                    apply_scales=apply_scales, apply_offsets=apply_offsets)
+
+            # data shape is (nchan, nsample)
+            # Truncate data to desired interval
+            if trunc > 0:
+                data = data[:, skip:-trunc]
+            elif trunc == 0:
+                data = data[:, skip:]
+            else:
+                raise ValueError("Number of bins to truncate is negative: %d" % trunc)
+
+            start    = start + block + skipback
+            data     = data.transpose().ravel()
+
+            yield int(block),int(ii),data
+
+        if verbose:
+            print("Execution time: %f seconds     \n"%(time.time()-tstart))
+
+
+
+
+    def get_data(self, startsub, endsub, apply_weights=False, 
+                        apply_scales=False, apply_offsets=False):
+        """
+        #FUTURE Work: Move this whole function in C
+        Source: https://github.com/devanshkv/your/blob/master/your/psrfits.py 
+        Return 2D array of data from PSRFITS file.
+
+        :param startsub: first subint to be read
+        :type startsub: int
+        :param endsub: last subint to read
+        :type endsub: int
+        """      
+        cumsum_num_subint = np.cumsum(self.specinfo.num_subint)
+        startfileid = np.where(startsub < cumsum_num_subint)[0][0]
+        assert startfileid < len(self.filelist)
+
+        if startfileid != self.fileid:
+            self.fileid = startfileid
+            self._fits.close()
+            del self._fits['SUBINT']
+            self.filename = self.filelist[self.fileid]
+            self._fits = pyfits.open(self.filename, mode='readonly', memmap=True)
+
+        # Read data
+        data = []
+        for isub in range(startsub, endsub + 1):
+            if isub > cumsum_num_subint[self.fileid] - 1:
+                self._fits.close()
+                del self._fits['SUBINT']
+                self.fileid += 1
+                if self.fileid == len(self.filelist):
+                    self.fileid-=1
+                    break
+                self.filename = self.filelist[self.fileid]
+                self._fits = pyfits.open(self.filename, mode='readonly', memmap=True)
+
+            fsub = int((isub - np.concatenate([np.array([0]),cumsum_num_subint]))[self.fileid])
+            try:
+                data.append(self.read_subint(fsub, apply_weights=apply_weights, 
+                                                   apply_scales=apply_scales, 
+                                                   apply_offsets=apply_offsets))
+            except KeyError:
+                self._fits = pyfits.open(self.filename, mode='readonly', memmap=True)
+                data.append(self.read_subint(fsub, apply_weights=apply_weights, 
+                                                   apply_scales=apply_scales, 
+                                                   apply_offsets=apply_offsets))
+
+        if len(data) > 1:
+            data = np.concatenate(data)
+        else:
+            data = np.array(data).squeeze()
+        data = np.transpose(data)                   # (nchan, nsample)
+        
+        #         if not self.specinfo.need_flipband:
+        # TODO
+
+        return data
+
+
+
+    def read_subint(self, isub, apply_weights=True, apply_scales=True, \
+                    apply_offsets=True):
+        """
+        Read a PSRFITS subint from a open pyfits file object.
+         Applys scales, weights, and offsets to the data.
+             Inputs: 
+                isub: index of subint (first subint is 0)
+                apply_weights: If True, apply weights. 
+                    (Default: apply weights)
+                apply_scales: If True, apply scales. 
+                    (Default: apply scales)
+                apply_offsets: If True, apply offsets. 
+                    (Default: apply offsets)
+             Output: 
+                data: Subint data with scales, weights, and offsets
+                     applied in float32 dtype with shape (nsamps,nchan).
+        """
+        sdata = self._fits['SUBINT'].data[isub]['DATA']      #(NSAMP_subint, NPOL, NCHAN)
+        shp = sdata.squeeze().shape
+        if self.specinfo.bits_per_sample < 8:  # Unpack the bytes data
+            if (shp[0] != self.specinfo.spectra_per_subint) and \
+                    (shp[1] != self.header.nchans * self.specinfo.bits_per_sample // 8):
+                sdata = sdata.reshape(self.specinfo.spectra_per_subint,
+                                 int(self.header.nchans * self.specinfo.bits_per_sample // 8))
+            if self.specinfo.bits_per_sample == 4:
+                data = unpack_4bit(sdata)
+            elif self.specinfo.bits_per_sample == 2:
+                data = unpack_2bit(sdata)
+            else:
+                data = np.asarray(sdata)
+        else:
+            # Handle 4-poln GUPPI/PUPPI data
+            if (len(shp) == 3 and shp[1] == self.specinfo.num_polns and
+                    self.specinfo.poln_order == "AABBCRCI"):
+                logger.warning("Polarization is AABBCRCI, summing AA and BB")
+                data = np.zeros((self.specinfo.spectra_per_subint,
+                                 self.header.nchans), dtype=np.float32)
+                data += sdata[:, 0, :].squeeze()
+                data += sdata[:, 1, :].squeeze()
+            elif (len(shp) == 3 and shp[1] == self.specinfo.num_polns and
+                  self.specinfo.poln_order == "IQUV"):
+                logger.warning("Polarization is IQUV, just using Stokes I")
+                data = np.zeros((self.specinfo.spectra_per_subint,
+                                 self.header.nchans), dtype=np.float32)
+                data += sdata[:, 0, :].squeeze()
+            else:
+                data = np.asarray(sdata)
+        data = data.reshape((self.spectra_per_subint,
+                             self.header.nchans)).astype(np.float32)
+        if apply_scales: data *= self.get_scales(isub)[:self.header.nchans]
+        if apply_offsets: data += self.get_offsets(isub)[:self.header.nchans]
+        if apply_weights: data *= self.get_weights(isub)[:self.header.nchans]
+        return data
+
+    def get_weights(self, isub):
+        """
+        Return weights for a particular subint.
+            Inputs:
+                isub: index of subint (first subint is 0)
+            
+            Output:
+                weights: Subint weights. (There is one value for each channel)
+        """
+        return self._fits['SUBINT'].data[isub]['DAT_WTS']
+
+    def get_scales(self, isub):
+        """
+        Return scales for a particular subint.
+            Inputs:
+                isub: index of subint (first subint is 0)
+            
+            Output:
+                scales: Subint scales. (There is one value for each channel)
+        """
+        return self._fits['SUBINT'].data[isub]['DAT_SCL']
+
+    def get_offsets(self, isub):
+        """
+        Return offsets for a particular subint.
+            Inputs:
+                isub: index of subint (first subint is 0)
+            
+            Output:
+                offsets: Subint offsets. (There is one value for each channel)
+        """
+        return self._fits['SUBINT'].data[isub]['DAT_OFFS']
+
+
+
 
 
 
